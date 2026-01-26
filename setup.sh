430af635b7ad93a5c72e724fc34cdb763b017ad9
chubbyhippo <mkmsacc@outlook.com>
2026-01-26 09:05:46 +0700

    Fix typo in URL variable name in `setup.sh`

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-RAW_DOOM_EMACS_SETTINGS_URL="https://raw.githubusercontent.com/chubbyhippo/doomemacs-settings/refs/heads/main/.config/doom"
+RAW_DOOMEMACS_SETTINGS_URL="https://raw.githubusercontent.com/chubbyhippo/doomemacs-settings/refs/heads/main/.config/doom"
 
-curl -ksS "$RAW_DOOM_EMACS_SETTINGS_URL/config.el" -o $HOME/.config/doom/config.el
-curl -ksS "$RAW_DOOM_EMACS_SETTINGS_URL/init.el" -o $HOME/.config/doom/init.el
+curl -ksS "$RAW_DOOMEMACS_SETTINGS_URL/config.el" -o $HOME/.config/doom/config.el
+curl -ksS "$RAW_DOOMEMACS_SETTINGS_URL/init.el" -o $HOME/.config/doom/init.el
