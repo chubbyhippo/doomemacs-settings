6cc60a5c1b4920412593a145134d53c98b009f3a
chubbyhippo <mkmsacc@outlook.com>
2026-01-26 12:16:33 +0700

    Add `packages.el` download to `setup.sh` script

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ RAW_DOOMEMACS_SETTINGS_URL="https://raw.githubusercontent.com/chubbyhippo/doomem
 
 curl -ksS "$RAW_DOOMEMACS_SETTINGS_URL/config.el" -o $HOME/.config/doom/config.el
 curl -ksS "$RAW_DOOMEMACS_SETTINGS_URL/init.el" -o $HOME/.config/doom/init.el
+curl -ksS "$RAW_DOOMEMACS_SETTINGS_URL/packages.el" -o $HOME/.config/doom/packages.el
